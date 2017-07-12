0bddca116a2115d57297bf29cbb75fb2a2aded7c
FinelifeX <random.finelife@gmail.com>
2017-07-12 18:49:26 +0300

    TabLayout + ViewPager homework for AndroidLab

---
+++ TabLayoutHomework/app/src/main/java/com/example/user/tablayouthomework/SimpleFragment.java
@@ -0,0 +1,26 @@
+package com.example.user.tablayouthomework;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Bulat Murtazin on 12.07.2017.
+ */
+
+public class SimpleFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_simple, container, false);
+    }
+}
